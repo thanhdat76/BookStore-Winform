2fa2497a5476e0e1078a676f8e42e8ed9068f6e3
NhatNam28092001 <nhatnamd14cnpm8@gmail.com>
2021-12-24 14:26:39 +0700

    form music moi lam

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -40,6 +40,7 @@ namespace BookStoreManagement
             this.btnNhanVien = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_music = new System.Windows.Forms.Button();
             this.panelMain.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -50,15 +51,17 @@ namespace BookStoreManagement
             this.panelMain.Controls.Add(this.panelShow);
             this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelMain.Location = new System.Drawing.Point(0, 0);
+            this.panelMain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawing.Size(1432, 953);
+            this.panelMain.Size = new System.Drawing.Size(1028, 609);
             this.panelMain.TabIndex = 0;
             // 
             // panelShow
             // 
-            this.panelShow.Location = new System.Drawing.Point(201, 141);
+            this.panelShow.Location = new System.Drawing.Point(151, 115);
+            this.panelShow.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelShow.Name = "panelShow";
-            this.panelShow.Size = new System.Drawing.Size(1228, 810);
+            this.panelShow.Size = new System.Drawing.Size(921, 658);
             this.panelShow.TabIndex = 0;
             // 
             // btnDoanhSo
@@ -67,9 +70,10 @@ namespace BookStoreManagement
             this.btnDoanhSo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(185)))), ((int)(((byte)(176)))));
             this.btnDoanhSo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDoanhSo.ForeColor = System.Drawing.Color.Red;
-            this.btnDoanhSo.Location = new System.Drawing.Point(10, 442);
+            this.btnDoanhSo.Location = new System.Drawing.Point(8, 359);
+            this.btnDoanhSo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDoanhSo.Name = "btnDoanhSo";
-            this.btnDoanhSo.Size = new System.Drawing.Size(191, 59);
+            this.btnDoanhSo.Size = new System.Drawing.Size(143, 48);
             this.btnDoanhSo.TabIndex = 10;
             this.btnDoanhSo.Text = "Doanh số";
             this.btnDoanhSo.UseVisualStyleBackColor = false;
@@ -81,9 +85,10 @@ namespace BookStoreManagement
             this.btnThueSach.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(185)))), ((int)(((byte)(176)))));
             this.btnThueSach.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnThueSach.ForeColor = System.Drawing.Color.Red;
-            this.btnThueSach.Location = new System.Drawing.Point(10, 357);
+            this.btnThueSach.Location = new System.Drawing.Point(8, 290);
+            this.btnThueSach.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnThueSach.Name = "btnThueSach";
-            this.btnThueSach.Size = new System.Drawing.Size(191, 59);
+            this.btnThueSach.Size = new System.Drawing.Size(143, 48);
             this.btnThueSach.TabIndex = 9;
             this.btnThueSach.Text = "Thuê sách";
             this.btnThueSach.UseVisualStyleBackColor = false;
@@ -95,9 +100,10 @@ namespace BookStoreManagement
             this.btnSach.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(185)))), ((int)(((byte)(176)))));
             this.btnSach.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSach.ForeColor = System.Drawing.Color.Red;
-            this.btnSach.Location = new System.Drawing.Point(10, 276);
+            this.btnSach.Location = new System.Drawing.Point(8, 224);
+            this.btnSach.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSach.Name = "btnSach";
-            this.btnSach.Size = new System.Drawing.Size(191, 59);
+            this.btnSach.Size = new System.Drawing.Size(143, 48);
             this.btnSach.TabIndex = 8;
             this.btnSach.Text = "Sách";
             this.btnSach.UseVisualStyleBackColor = false;
@@ -109,9 +115,10 @@ namespace BookStoreManagement
             this.btnHoaDon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(185)))), ((int)(((byte)(176)))));
             this.btnHoaDon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnHoaDon.ForeColor = System.Drawing.Color.Red;
-            this.btnHoaDon.Location = new System.Drawing.Point(10, 192);
+            this.btnHoaDon.Location = new System.Drawing.Point(8, 156);
+            this.btnHoaDon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnHoaDon.Name = "btnHoaDon";
-            this.btnHoaDon.Size = new System.Drawing.Size(191, 59);
+            this.btnHoaDon.Size = new System.Drawing.Size(143, 48);
             this.btnHoaDon.TabIndex = 7;
             this.btnHoaDon.Text = "Hóa đơn";
             this.btnHoaDon.UseVisualStyleBackColor = false;
@@ -123,9 +130,10 @@ namespace BookStoreManagement
             this.btnDoiTac.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(185)))), ((int)(((byte)(176)))));
             this.btnDoiTac.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDoiTac.ForeColor = System.Drawing.Color.Red;
-            this.btnDoiTac.Location = new System.Drawing.Point(10, 108);
+            this.btnDoiTac.Location = new System.Drawing.Point(8, 88);
+            this.btnDoiTac.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDoiTac.Name = "btnDoiTac";
-            this.btnDoiTac.Size = new System.Drawing.Size(191, 59);
+            this.btnDoiTac.Size = new System.Drawing.Size(143, 48);
             this.btnDoiTac.TabIndex = 6;
             this.btnDoiTac.Text = "Đối tác";
             this.btnDoiTac.UseVisualStyleBackColor = false;
@@ -138,9 +146,10 @@ namespace BookStoreManagement
             this.btnNhanVien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNhanVien.ForeColor = System.Drawing.Color.Red;
             this.btnNhanVien.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnNhanVien.Location = new System.Drawing.Point(10, 29);
+            this.btnNhanVien.Location = new System.Drawing.Point(8, 24);
+            this.btnNhanVien.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnNhanVien.Name = "btnNhanVien";
-            this.btnNhanVien.Size = new System.Drawing.Size(191, 59);
+            this.btnNhanVien.Size = new System.Drawing.Size(143, 48);
             this.btnNhanVien.TabIndex = 5;
             this.btnNhanVien.Text = "Nhân viên";
             this.btnNhanVien.UseVisualStyleBackColor = false;
@@ -150,35 +159,54 @@ namespace BookStoreManagement
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(135)))), ((int)(((byte)(84)))));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btn_music);
             this.panel1.Controls.Add(this.btnDoanhSo);
             this.panel1.Controls.Add(this.btnThueSach);
             this.panel1.Controls.Add(this.btnNhanVien);
             this.panel1.Controls.Add(this.btnSach);
             this.panel1.Controls.Add(this.btnDoiTac);
             this.panel1.Controls.Add(this.btnHoaDon);
-            this.panel1.Location = new System.Drawing.Point(-7, 140);
+            this.panel1.Location = new System.Drawing.Point(-5, 114);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(206, 812);
+            this.panel1.Size = new System.Drawing.Size(155, 660);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-7, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(-5, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1437, 140);
+            this.pictureBox1.Size = new System.Drawing.Size(1078, 114);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
+            // btn_music
+            // 
+            this.btn_music.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btn_music.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(185)))), ((int)(((byte)(176)))));
+            this.btn_music.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_music.ForeColor = System.Drawing.Color.Red;
+            this.btn_music.Location = new System.Drawing.Point(8, 421);
+            this.btn_music.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_music.Name = "btn_music";
+            this.btn_music.Size = new System.Drawing.Size(143, 48);
+            this.btn_music.TabIndex = 11;
+            this.btn_music.Text = "Music";
+            this.btn_music.UseVisualStyleBackColor = false;
+            this.btn_music.Click += new System.EventHandler(this.btn_music_Click);
+            // 
             // Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1432, 953);
+            this.ClientSize = new System.Drawing.Size(1028, 609);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panelMain);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Home";
             this.Text = "Home";
             this.panelMain.ResumeLayout(false);
@@ -200,5 +228,6 @@ namespace BookStoreManagement
         private System.Windows.Forms.Button btnHoaDon;
         private System.Windows.Forms.Button btnDoiTac;
         private System.Windows.Forms.Button btnNhanVien;
+        private System.Windows.Forms.Button btn_music;
     }
 }
